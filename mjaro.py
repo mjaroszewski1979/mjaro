aca58525c510aa66edab9a07a916372a0975a861
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2020-09-28 16:50:58 +0200

    Update mjaro.py

--- mjaro.py
+++ mjaro.py
@@ -11,6 +11,10 @@ port = int(os.environ.get('PORT', 5000))
 def index():
     return '<h1>Hello</h1>'
 
+@app.route('/about')
+def about():
+    return '<h1>About</h1>'
+
 if __name__ == '__main__':
    port = int(os.environ.get("PORT", 5000))
    app.run(host='0.0.0.0', port=port, debug=True)
